f5dee1f1a94bb43c31b7c43cebb2749203e9310a
sjouk jonker <sjoukjonker13@gmail.com>
2025-06-18 10:55:01 +0200

    update

--- voetbalapp/src/app/app.routes.ts
+++ voetbalapp/src/app/app.routes.ts
@@ -2,6 +2,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 
+
 export const routes: Routes = [
-    
+    {
+        path: '',
+        component: Home
+    }
 ];
