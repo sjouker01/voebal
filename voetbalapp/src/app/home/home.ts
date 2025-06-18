8657c8e475a9e7dcbb930e82d44b3a3a7abfd4e3
sjouk jonker <sjoukjonker13@gmail.com>
2025-06-18 11:22:11 +0200

    s

--- voetbalapp/src/app/home/home.ts
+++ voetbalapp/src/app/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HomeApi } from './home-api';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   imports: [CommonModule],
