400a3702b4d77eda7960ccad1e2a50b5811db790
sjouk jonker <sjoukjonker13@gmail.com>
2025-06-18 11:17:53 +0200

    update

--- voetbalapp/src/app/home/home.ts
+++ voetbalapp/src/app/home/home.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HomeApi } from './home-api';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+  years: any[] = [];
+
+  constructor(private homeService: HomeApi) {
+    this.homeService.getData().subscribe((data:any) =>{
+    this.years = data.Search;
+    }
+    )
+  }
 
 }
