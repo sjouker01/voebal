99a7bb3808dc47c7b065b79634efcd86b01123eb
sjouk jonker <sjoukjonker13@gmail.com>
2025-06-19 14:08:41 +0200

    id
    id is door gegeven naar ander pagina

--- voetbalapp/src/app/toernooi-details/details-api.ts
+++ voetbalapp/src/app/toernooi-details/details-api.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailsApi {
-
-  constructor() { }
+ 
 }
--- voetbalapp/src/app/toernooi-details/toernooi-details.ts
+++ voetbalapp/src/app/toernooi-details/toernooi-details.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-
 @Component({
   selector: 'app-toernooi-details',
   imports: [],
   templateUrl: './toernooi-details.html',
-  styleUrl: './toernooi-details.scss'
+  styleUrl: './toernooi-details.scss',
 })
+
+// this.id is id of home page (id = ook jaar )
 export class ToernooiDetails {
-constructor(private route: ActivatedRoute){
-  this.route.params.subscribe(params => console.log(params))
+  id: string | null = null;
+  filter: string | null = null;
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParamMap.subscribe((params) => {
+      this.id = params.get('id');
+      console.log(this.id);
+    });
+  }
 }
-}
-  
