8146c3513f25fd89b554c191dfade50c7f98298a
sjouk jonker <sjoukjonker13@gmail.com>
2025-06-22 20:28:57 +0200

    start styling

--- voetbalapp/src/app/toernooi-details/toernooi-details.ts
+++ voetbalapp/src/app/toernooi-details/toernooi-details.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 import { DetailsApi } from './details-api';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-toernooi-details',
-  imports: [CommonModule , RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './toernooi-details.html',
   styleUrl: './toernooi-details.scss',
 })
@@ -15,26 +16,30 @@ import { RouterModule } from '@angular/router';
 export class ToernooiDetails {
   id: string | null = null;
   filter: string | null = null;
-  matches: any[] =[]; 
+  matches: any[] = [];
   games: any[] = [];
-  constructor(private route: ActivatedRoute , private matchesService:DetailsApi) {
+  constructor(
+    private route: ActivatedRoute,
+    private matchesService: DetailsApi,
+    private location: Location
+  ) {
     this.route.queryParamMap.subscribe((params) => {
       this.id = params.get('id');
       // console.log(this.id);
-      this.matchesService.getMatches().subscribe((data:any) => {
+      this.matchesService.getMatches().subscribe((data: any) => {
         this.matches = data;
         // console.log( this.matches);
         this.filterMatches();
         console.log(this.games);
-      })
-      
+      });
     });
   }
   filterMatches(): void {
-   
-   this.games = this.matches.filter(match => match.year === this.id);
-   console.log(this.games);
-   
-   
- }
+    this.games = this.matches.filter((match) => match.year === this.id);
+    console.log(this.games);
+  }
+    goBack(){
+    this.location.back();
+  }
+
 }
